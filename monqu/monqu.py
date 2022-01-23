3c901c5020a4572c0002928a61bc310276e04145
specd-tech <specdtech@protonmail.ch>
2022-01-23 03:20:27 -0500

    Consolidated gevent classes to one file

--- monqu/monqu.py
+++ monqu/monqu.py
@@ -1,11 +1,13 @@
 from gevent.monkey import patch_all; patch_all()
 import gevent
+from gevent import wait
 from gevent.pool import Pool
 from cloudpickle.cloudpickle_fast import dumps
 from pymongo import MongoClient
 from bson import Binary
 from typing import Union, Callable
 from functools import wraps
+from base_worker import BaseWorker
 
 
 # make sure _tasks is correct and doesn't need to be Pool()
@@ -91,3 +93,51 @@ class MonquServer:
 
     def wait(self):
         gevent.joinall(self._tasks)
+
+
+class GeventWorker(BaseWorker):
+    def __init__(
+            self,
+            mongo_connection: str,
+            database: str = 'monqu',
+            queue: str = 'queue',
+            greenlet_threads: int = 10,
+            prefetch: int = 0
+    ):
+        super().__init__(mongo_connection, database, queue)
+        self.task_pool = Pool(greenlet_threads)
+        # change to task pool
+        self._task_pool = list()
+        # add or * greenlet_threads for prefetch
+        self.prefetch = greenlet_threads + prefetch
+        # self.prefetch_pool = Pool(self.prefetch)
+        # self._prefetch_pool = list()
+
+    def wait(self):
+        wait(self._task_pool)
+
+    def worker(self, order: str = 'fifo'):
+        # add timer
+        # add patterning matching
+        # Add pause logic
+        get_func = self.fifo
+        while True:
+            left = self.prefetch - len(self._local_queue)
+
+            for _ in range(left):
+                if func := get_func():
+                    self._local_queue.append(func)
+
+                elif func is None and self._local_queue == []:
+                    self.watch()
+
+                else:
+                    break
+
+            for func in self._local_queue:
+                self._task_pool.append(self.task_pool.spawn(self.call_func, func))
+                self._local_queue.remove(func)
+                # Test to see if this loop blocks while waiting and if this is nessary
+                # maybe put at top of for loop
+                if self.task_pool.full():
+                    break
